8e4df57e52f58db187eb316bdfc8632bfa55431c
Malkiel Asher <masher@syr.edu>
2024-12-17 14:50:14 -0500

    Fixed DB query

--- DBScripts/DBQueries.sql
+++ DBScripts/DBQueries.sql
@@ -1,14 +1,14 @@
 CREATE TABLE CandidateProfile (
-    Id INT IDENTITY(1,1) PRIMARY KEY,
-    FirstName NVARCHAR(50) NOT NULL,
-    LastName NVARCHAR(50) NOT NULL,
-    Email NVARCHAR(50) NOT NULL UNIQUE,
-    Education NVARCHAR(100),
-    yearOfGradution NVARCHAR(4),
-    funFact NVARCHAR(200)
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    first_name NVARCHAR(50) NOT NULL,
+    last_name NVARCHAR(50) NOT NULL,
+    email NVARCHAR(50) NOT NULL UNIQUE,
+    education NVARCHAR(100),
+    year_of_gradution NVARCHAR(4),
+    fun_fact NVARCHAR(200)
 );
 
-INSERT INTO dbo.CandidateProfile (FirstName, LastName, Email, Education, yearOfGradution, funFact) 
+INSERT INTO dbo.CandidateProfile (first_name, last_name, email, education, year_of_gradution, fun_fact) 
 VALUES 
 ('John', 'Doe', 'john.doe@example.com', 'Bachelor of Science', 2020, 'Loves hiking.'),
 ('Jane', 'Smith', 'jane.smith@example.com', 'Master of Arts', 2019, 'Plays the guitar.'),
