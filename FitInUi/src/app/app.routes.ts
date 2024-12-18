b930002d71ee1910009bdf597aba00bad7fd3d1c
Malkiel Asher <masher@syr.edu>
2024-12-18 18:33:54 -0500

    Adding Sangati's Routes

--- FitInUi/src/app/app.routes.ts
+++ FitInUi/src/app/app.routes.ts
@@ -2,6 +2,9 @@ import { provideRouter, Route } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TeamComponent } from './team/team.component';
+import { CandidateComponent } from './candidate/candidate.component';
 
 export const routes: Route[] = [
     { 
@@ -17,6 +20,8 @@ export const routes: Route[] = [
         redirectTo: '/login', 
         pathMatch: 'full' 
     },
+    { path: 'team', component: TeamComponent },
+    { path: 'candidate', component: CandidateComponent  }
   ];
 
   export class AppRoutingModule{}
